fe62fce89a3154d1705df2b598dabd798805cfc5
Allan <allannfl@hotmail.com>
2016-09-08 19:14:21 -0300

    classes funcionario e bicicleta

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Funcionario
+from .models import Bicicleta
+
+class FuncionarioAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'telefone', 'endereco', 'funcao', 'codigo')
+
+class BicicletaAdmin(admin.ModelAdmin):
+	list_display = ('marca', 'modelo', 'cor', 'descricao', 'situacao')
+
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Bicicleta, BicicletaAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Funcionario(models.Model):
+	nome = models.CharField(max_length=40)
+	endereco = models.CharField(max_length=60)
+	telefone = models.CharField(max_length=15)
+	codigo = models.CharField(max_length=10)
+	funcao = models.CharField(max_length=40)
+
+
+	def __str__(self):
+		return self.nome
+
+
+class Bicicleta(models.Model):
+	marca = models.CharField(max_length=40)
+	modelo = models.CharField(max_length=40)
+	cor = models.CharField(max_length=20)
+	descricao = models.CharField(max_length=100)
+	situacao = models.CharField(max_length=2)
+
+
+	def __str__(self):
+		return self.modelo
